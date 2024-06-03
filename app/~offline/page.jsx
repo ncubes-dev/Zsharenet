7ee90829611751cb312e995493702c5f9872c6a5
ncubes <ncubes1999@gmail.com>
2024-06-03 17:50:19 +0200

    added offline and awaiting testing

--- app/~offline/page.jsx
+++ app/~offline/page.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import Offline from '../offline'
+'use client'
+import animationData from './no_network'
+import Lottie from 'react-lottie'
 
 const Offline = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  }
   return (
-    <div>
-      <Offline />
+    <div className='w-full h-full inset-0'>
+      <Lottie options={defaultOptions} height={200} width={200} />
+      <h1 className='text-red text-center font-extrabold text-xl mx-3'>
+        Network connection is needed to download this page
+      </h1>
     </div>
   )
 }
